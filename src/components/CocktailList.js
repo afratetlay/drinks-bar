3d04b5ab92bd2bb8e26ed293ed9bb430cb558d40
Michal Jasinski <michal.jasinski@xigen.co.uk>
2022-01-13 23:28:29 +0100

    Progres with build

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -11,50 +11,27 @@ export default class CocktailList extends React.Component {
             cocktails: []
         };
     }
-    
-    
-
 
     componentDidMount() {
-        
-        // toggleState = () => {
-    
-        //     axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=gin`)
-        //         .then(res => {
-        //             console.log(res);
-        //             const cocktails = res.data.drinks;
-        //             this.setState({ cocktails });
-        //         })
-        // }
-    }
-
-    toggleState = (e) => {
-
-        // const btnValue = this.props.value;
-        console.log(e.target.val);
-    
-        // axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${btnValue}`)
-        //     .then(res => {
-        //         console.log(res);
-        //         const cocktails = res.data.drinks;
-        //         this.setState({ cocktails });
-        //     })
+        axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=gin`)
+            .then(res => {
+                console.log(res);
+                const cocktails = res.data.drinks;
+                this.setState({ cocktails });
+            })
     }
 
     render() {
         return (
             <div className="cocktail-list">
-                <button onClick={this.toggleState} val="gin">Gin</button>
-                {/* <button onClick={this.toggleState} value="vodka">Vodka</button>
-                <button onClick={this.toggleState} value="whisky">Whisky</button> */}
-                <Container>  
-                    <Row>
-                    
-                    {this.state.cocktails.map((cocktail) => {
-                        return <div className="col-6"> <Cocktail  name={cocktail.strDrink} thumb={cocktail.strDrinkThumb} key={cocktail.idDrink} /> </div>
-                    })}
-                    </Row>  
-                </Container>    
+            <Container>  
+                <Row>
+                  
+                {this.state.cocktails.map((cocktail) => {
+                    return <div className="col-6"> <Cocktail  name={cocktail.strDrink} thumb={cocktail.strDrinkThumb} key={cocktail.idDrink} /> </div>
+                })}
+                </Row>  
+            </Container>    
             </div>
         );
     }
