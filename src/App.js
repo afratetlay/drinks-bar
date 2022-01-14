3eb7542c29730ca1d2425628687bd65ee47a5b6a
Michal Jasinski <michal.jasinski@xigen.co.uk>
2022-01-14 12:46:06 +0100

    Merge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CocktailList from './components/CocktailList';
-import Product from './components/Product';
+// import Product from './components/Product';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
